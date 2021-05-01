cd4f90d87888b5c68541e3c3a2bf0874d8761637
Adison25 <emerickadison@gmail.com>
2021-04-30 18:52:26 -0700

    forgot to have test_

--- testCalc.py
+++ testCalc.py
@@ -2,25 +2,25 @@ import unittest
 import calc
 
 class TestCase(unittest.TestCase):
-    def testAdd(self):
+    def test_Add(self):
         self.assertEqual(calc.addition(2, 1),3)
     #Fail Add
-    #def test2(self):
+    #def test_2(self):
         #self.assertEqual(calc.addition(2, 1),4)
-    def testSub(self):
+    def test_Sub(self):
         self.assertEqual(calc.subtraction(2, 1),1)
     #Fail Sub
-    #def test4(self):
+    #def test_4(self):
         #self.assertEqual(calc.subtraction(2, 1),3)
-    def testMult(self):
+    def test_Mult(self):
         self.assertEqual(calc.multiplication(2, 1),2)
     #Fail multiplication
-    #ef test6(self):
+    #ef test_6(self):
         #self.assertEqual(calc.multiplication(2, 1),3)
-    def testDiv(self):
+    def test_Div(self):
         self.assertEqual(calc.division(2, 1),2)
     #Fail division
-    #def test8(self):
+    #def test_8(self):
         #self.assertEqual(calc.division(2, 1),3)
 
 if __name__ == '__main__':
